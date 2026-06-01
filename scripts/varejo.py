8bc6d9561071389c44b4a14973c6243ad6806d74
Luciazev <lazevedo40@yahoo.com>
2026-06-01 15:10:24 -0300

    Organizaçao e Limpeza do Varejo.py

--- scripts/varejo.py
+++ scripts/varejo.py
@@ -1,5 +1,12 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 df = pd.read_csv('Dados/Base Varejo.csv', sep=';')
+#LIMPEZA
+df = df.drop_duplicates()
+df['DATA'] = pd.to_datetime(df['DATA'],dayfirst=True, errors='coerce')
+df['PR_CAT'] = df['PR_CAT'].replace('#N/D','Desconhecido')
+df = df.drop(columns=[col for col in df.columns if "Unnamed" in col])
+#Analise exploratoria
 print('Base carregada!')
 print(df.shape)
 print('\ncolunas')
@@ -32,6 +39,7 @@ datas_invalidas = df['DATA'].isnull().sum()
 print('\ntotal datas invalidas')
 print(datas_invalidas)
 df = df.drop_duplicates()
+df['PR_CAT'] = df['PR_CAT'].replace('#N/D', 'Desconhecido')
 print("\nNovo tamanho após remover duplicatas:")
 print(df.shape)
 print(df.columns)
@@ -39,31 +47,27 @@ print("\nESTATISTICA DESCRITIVA - FILHOS")
 print("contagem:", df['CL_FHL'].count())
 print("media:", df['CL_FHL'].mean())
 print("mediana:", df['CL_FHL'].median())
-print("Moda:", df['CL_FHL'].mode())
+print("Moda:", df['CL_FHL'].mode()[0])
 print("desvio padrao:", df['CL_FHL'].std())
 print("minimo:", df['CL_FHL'].min())
 print("maximo:", df['CL_FHL'].max())
 print("\nQuartis:")
 print(df['CL_FHL'].describe())
-import matplotlib.pyplot as plt
 #df["CL_FHL"].hist(bins=10)
 #plt.title("distribuição do numero de filhos(CL_FHL)")
 #plt.xlabel("numero de filhos")
 #plt.ylabel("frequencia")
 #plt.show()
-import matplotlib.pyplot as plt
-#plt.show()
-df.columns
 print("\n=== Quantidade de clientes por genero ===")
 print(df['CL_GENERO'].value_counts())
-print('cheguei aqui')
+#Estatistica Descritiva
 print(df['CL_FHL'].describe())
-print('vou contar generos')
 print(df['CL_GENERO'].value_counts())
 print("\n=== COLUNAS DA BASE ===")
 print(df.columns.tolist())
 df = df.drop(columns=[col for col in df.columns if "Unnamed" in col])
 print("\n=== Clientes por genero ===")
+#Agrupamento
 print(df.groupby("CL_GENERO")["CL_ID"].nunique())
 print("\n=== Media de filhos por genero ===")
 print(df.groupby("CL_GENERO")["CL_FHL"].mean())
@@ -75,6 +79,8 @@ print("\n=== Categorias mais vendidas ===")
 print(df["PR_CAT"].value_counts())
 print("\n=== vendas por categoria ===")
 print(df.groupby("PR_CAT")["CO_ID"].count())
+
+ 
 
 
 
